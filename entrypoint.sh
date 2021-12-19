219e005eef39f1de691d7187cc7b6b9aa4071355
Sebastian Wieland <git-commits@nwie.land>
2021-12-19 09:41:45 +0100

    no local

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ chmod 600 /tmp/id_ssh
 
 # Set up client authorization
 if [[ -n "$INPUT_ONION_CLIENT_AUTH_PRIVATEKEY" ]]; then
-  local authdir=/var/lib/tor/onion_auth
+  authdir=/var/lib/tor/onion_auth
   echo "ClientOnionAuthDir $authdir" >> /etc/tor/torrc
   mkdir -p $authdir
   echo "$INPUT_ONION_HOST:descriptor:x25519:$INPUT_ONION_CLIENT_AUTH_PRIVATEKEY" \
@@ -22,7 +22,7 @@ fi
 service tor start
 
 # Set up ssh options
-local ssh_opts="ssh -i /tmp/id_ssh -p $INPUT_SSH_PORT"
+ssh_opts="ssh -i /tmp/id_ssh -p $INPUT_SSH_PORT"
 
 # Strict host key checking
 if [[ ( -n "$INPUT_SSL_DISABLE_STRICT_HOST_KEY_CHECKING" ) && ( "$INPUT_SSL_DISABLE_STRICT_HOST_KEY_CHECKING" = "true" )]]; then
@@ -33,7 +33,7 @@ fi
 
 # SSH Host fingerprint
 if [[ -n "$SSH_HOST_FINGERPRINT" ]]; then
-  local hosts_file="/tmp/known_hosts"
+  hosts_file="/tmp/known_hosts"
   echo "$SSH_HOST_FINGERPRINT" > $hosts_file
   ssh_opts="$ssh_opts -o UserKnownHostsFile=$hosts_file"
   
@@ -41,7 +41,7 @@ if [[ -n "$SSH_HOST_FINGERPRINT" ]]; then
 fi
 
 # Actual file synchronisation
-local destination="$INPUT_SSH_USER@$INPUT_ONION_HOST.onion:$INPUT_DESTINATION_DIR"
+destination="$INPUT_SSH_USER@$INPUT_ONION_HOST.onion:$INPUT_DESTINATION_DIR"
 
 if [[ ( -n "$INPUT_DELETE" ) && ( "$INPUT_DELETE" = "true" ) ]]; then
   torsocks rsync -rlptvz -e "$ssh_opts" --delete "$INPUT_SOURCE_DIR" "$destination"
